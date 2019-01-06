70d8afd1c0699c3b4ac9cd82ea9a1e166b0851ce
Patrick Dufour <pjdufour.dev@gmail.com>
2019-01-05 16:29:04 -0800

    fix

--- railgun/handlers/ItemHandler.go
+++ railgun/handlers/ItemHandler.go
@@ -50,7 +50,7 @@ func (h *ItemHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				panic(err)
 			}
 		} else {
-			err = h.RespondWithObject(w, http.StatusOK, obj, "", format)
+			err = h.RespondWithObject(w, http.StatusOK, obj, format, "")
 			if err != nil {
 				h.Messages <- err
 				err = h.RespondWithError(w, err, format)
@@ -72,7 +72,7 @@ func (h *ItemHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				panic(err)
 			}
 		} else {
-			err = h.RespondWithObject(w, http.StatusOK, obj, "", format)
+			err = h.RespondWithObject(w, http.StatusOK, obj, format, "")
 			if err != nil {
 				h.Messages <- err
 				err = h.RespondWithError(w, err, format)
@@ -94,7 +94,7 @@ func (h *ItemHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				panic(err)
 			}
 		} else {
-			err = h.RespondWithObject(w, http.StatusOK, obj, "", format)
+			err = h.RespondWithObject(w, http.StatusOK, obj, format, "")
 			if err != nil {
 				h.Messages <- err
 				err = h.RespondWithError(w, err, format)
